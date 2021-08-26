3e761747fc11632b946a390a6cb8c422845f523e
Shoaib Sharif <shoaib19j@gmail.com>
2021-08-25 22:26:45 -0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ["**/*.{js,ts,html,scss}"],
+  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
